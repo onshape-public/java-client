50dffd6b53a3f844b4f2b9d97703c384c64465ee
Ethan Keller <ethan.keller@gmail.com>
2019-03-26 17:05:18 -0400

    Include an upload file test that currently fails due to multipart/form-data not being included in the signature calculation

--- src/test/java/com/onshape/api/BlobElementsTest.java
+++ src/test/java/com/onshape/api/BlobElementsTest.java
@@ -22,33 +22,89 @@
 //
 package com.onshape.api;
 
+import com.onshape.api.desktop.OnshapeDesktop;
 import com.onshape.api.exceptions.OnshapeException;
+
+import java.io.File;
 import java.lang.String;
+
+import com.onshape.api.responses.BlobElementsUploadFileCreateElementResponse;
+import com.onshape.api.responses.DocumentsCreateDocumentResponse;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public final class BlobElementsTest {
-  private static Onshape CLIENT;
+    private static Onshape CLIENT;
 
-  public Onshape getClient() {
-    return CLIENT;
-  }
+    public Onshape getClient() {
+        return CLIENT;
+    }
 
-  public BlobElements getGroup() {
-    return getClient().blobElements();
-  }
+    public BlobElements getGroup() {
+        return getClient().blobElements();
+    }
 
-  public String getUserId() throws OnshapeException {
-    return getClient().users().getSessionInfo().call().getId();
-  }
+    public String getUserId() throws OnshapeException {
+        return getClient().users().getSessionInfo().call().getId();
+    }
 
-  @BeforeAll
-  public static void setup() throws OnshapeException {
-    CLIENT = new Onshape();
-    String accessKey = System.getenv("ONSHAPE_API_ACCESSKEY");
+    @Test
+    public void testUploadFileCreateElementWithOAuth() {
+        Onshape o = new Onshape();
+        OnshapeDesktop od = new OnshapeDesktop(System.getenv("ONSHAPE_CLIENTID"), System.getenv("ONSHAPE_CLIENTSECRET"));
+        try {
+            od.setupClient(o);
+        } catch (OnshapeException ex) {
+            Assertions.fail("Failed to set OAuth credentials", ex);
+            return;
+        }
+        uploadFileCreateElement(o);
+    }
+
+    @Test
+    public void testUploadFileCreateElementWithAPIKeys() throws OnshapeException {
+        Onshape o = new Onshape();
+        o.setAPICredentials(System.getenv("ONSHAPE_API_ACCESSKEY"), System.getenv("ONSHAPE_API_SECRETKEY"));
+        uploadFileCreateElement(o);
+    }
+
+    public void uploadFileCreateElement(Onshape o) {
+        File f = new File(new File(System.getProperty("user.dir")), "/src/test/resources/stepexample.step");
+        DocumentsCreateDocumentResponse createdDocument;
+        try {
+            createdDocument = o.documents().createDocument().isPublic(Boolean.TRUE).name("IAmATestDeleteMe").ownerType(0).call();
+        } catch (OnshapeException ex) {
+            Assertions.fail("Failed to create document", ex);
+            return;
+        }
+        BlobElementsUploadFileCreateElementResponse response;
+        try {
+            response = o.blobElements()
+                    .uploadFileCreateElement()
+                    .file(f)
+                    .translate(Boolean.TRUE)
+                    .yAxisIsUp(Boolean.FALSE)
+                    .flattenAssemblies(Boolean.FALSE)
+                    .call(createdDocument.getId(), createdDocument.getDefaultWorkspace().getId());
+        } catch (OnshapeException ex) {
+            Assertions.fail("Failed to upload blob to new element", ex);
+        }
+        try {
+            o.documents().deleteDocument().call(createdDocument.getId());
+        } catch (OnshapeException ex) {
+            Assertions.fail("Failed to delete document", ex);
+        }
+    }
+
+    @BeforeAll
+    public static void setup() throws OnshapeException {
+        CLIENT = new Onshape();
+        String accessKey = System.getenv("ONSHAPE_API_ACCESSKEY");
         String secretKey = System.getenv("ONSHAPE_API_SECRETKEY");
         if ((accessKey == null || accessKey.isEmpty()) || (secretKey == null || secretKey.isEmpty())) {
             throw new OnshapeException("Please define ONSHAPE_API_ACCESSKEY and ONSHAPE_API_SECRETKEY environment variables");
         }
         CLIENT.setAPICredentials(accessKey, secretKey);
-  }
+    }
 }
